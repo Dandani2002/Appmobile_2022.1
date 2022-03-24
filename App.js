6a85f34f008be312e05d918406b458281179ccb2
Dandani2002 <danielcavalcanteoficial@hotmail.com>
2022-03-24 10:28:39 -0300

    recoloquei todos os arquivos perdidos

--- App.js
+++ App.js
@@ -1,44 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Image, View } from 'react-native';
-import { ImageBackground, SafeAreaView } from 'react-native-web';
-
-import header from './src/assets/image1.jpeg';
-import xbox from './src/assets/xbox.jpg';
+import React from "react";
+import { StyleSheet, Text, View, Button } from 'react-native';
+import C1 from './componentes/comp1'
+import {Comp2} from './componentes/comp2'
+import { play } from "react-native-vector-icons/AntDesign";
 
 export default function App() {
   return (
-    <SafeAreaView >
-      <ImageBackground blurRadius={1.5} source={header} style={styles.header}>
-        <Text style={{color: "#FFF", fontSize: 50}}>Estácio</Text>
-      </ImageBackground>
-      <View style={styles.detailsView}>
-        <Text>Xbox Series Xbox</Text>
-        <Image source={xbox} style={{width: 50, height: 50}}></Image>
-        <Image source={{uri: 'https://a-static.mlcdn.com.br/1500x1500/console-playstation-5-ps5-sony/magazineluiza/043079500/9d4f0cd7244929620b459cf9fd5e471c.jpg'}} style={{width: 50, height: 50}}/>
-        <Text>Sony</Text>
-        <Text>Lorem ipsum dolor sit amet conseqt adiscipli elit</Text>
-        <Text>R$ 4.000,00</Text>
+      <View>
+     <><C1 /><Comp2 /></>
       </View>
-    </SafeAreaView>
+        
+      
+        
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    width:'100%', 
-    height: 300, 
-    display: 'flex', 
-    justifyContent: 'center', 
-    alignItems: 'center'
-  },
-  detailsView: {
-    paddingVertical: 10,
-    paddingHorizontal: 10
-  }
-});
+};
---
+++ componentes/comp1.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function Comp1(){
+    return(
+        <View style={styles.container}>
+        <Text style={styles.title}>Metrônomo</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        display: "flex",
+        justifyContent: "top",
+        alignItems: "center",
+        flex: 1,
+        padding: 24,
+        backgroundColor: "#eaeaea"
+    },
+      title: {
+        marginTop: 20,
+        paddingVertical: 4,
+        borderWidth: 3,
+        borderColor: "#20232a",
+        borderRadius: 6,
+        backgroundColor: "#2786f2",
+        color: "#20232a",
+        textAlign: "center",
+        fontSize: 40,
+        fontWeight: "bold"
+      }
+    });
